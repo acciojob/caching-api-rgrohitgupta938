e0e093593bdb37a3e0a1c7f5bd92a93d81340e24
Rohit Gupta <49370602+rgrohitgupta938@users.noreply.github.com>
2024-09-26 14:22:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,15 +3,28 @@ const fetchButton = document.getElementById("fetch-button");
 const resultsDiv = document.getElementById("results");
 
 const cache = new Map();
+const cachedTime = 60000;
+const cached = new Map();
 
 const fetchData = async () => {
   // implement the caching here and store data in cache variable
+	const cacheKey = "validData";
+
+	if(cached.has(cacheKey)){
+		const data = cached.get(cacheKey;
+		const currTime = Date.now();
+
+		console.log(data);
+		if(currTime - data.timestamp < cachedTime){
+			return data.data
+		}
+	}
 
   console.log("Making API call");
   const response = await fetch("https://opentdb.com/api.php?amount=3");
   const data = await response.json();
 
-  cache.set(cacheKey, {
+  cached.set(cacheKey, {
     timestamp: Date.now(),
     data: data,
   });
